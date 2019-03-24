aca870f02d6cea7f1d020e0d7b6cbcb707b8d54f
clara <10088152+claraj@users.noreply.github.com>
2019-03-23 21:26:43 -0500

    url -> path

--- lmnop_project/urls.py
+++ lmnop_project/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.edit import CreateView
@@ -22,12 +22,8 @@ from lmn import views, views_users
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),    #Admin site
+    path('admin/', admin.site.urls),    #Admin site
 
-    url(r'^accounts/login/$', auth_views.login, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
-    url(r'^register/$', views_users.register, name='register'),
-
-    url(r'^', include('lmn.urls') ),
+    path('', include('lmn.urls') ),
 
 ]
