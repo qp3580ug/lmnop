bf52f6bb7075d92767489c66f40ecdcc070c1544
Nathan Walter <qp3580ug@go.minnstate,edu>
2019-11-15 09:37:31 -0600

    Added a form to edit the users profile information.

--- lmnop_project/settings.py
+++ lmnop_project/settings.py
@@ -91,13 +91,13 @@ DATABASES = {
     # When you use Postgres, comment out or remove this DB config. 
     
     'default': {
-        """'ENGINE': 'django.db.backends.sqlite3',"""
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jhfpkmbg',
-        'USER': 'jhfpkmbg',
-        'PASSWORD': os.environ['LMNOP_DB_PW'],
-        'HOST': 'salt.db.elephantsql.com',
-        'PORT': '5432',
+        #'USER': 'jhfpkmbg',
+        #'PASSWORD': os.environ['LMNOP_DB_PW'],
+        #'HOST': 'salt.db.elephantsql.com',
+        #'PORT': '5432',
     }
 }
 
