48e2e211a2316344abd7c1b5c33e0ca3d70b1e08
Nathan Walter <qp3580ug@go.minnstate,edu>
2019-11-18 14:48:03 -0600

    fixed error on password reset

--- lmn/views_users.py
+++ lmn/views_users.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import render, redirect
 
 from django.utils import timezone
 
@@ -55,7 +54,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)
             messages.success(request, 'Your password was successfully updated!')
-            return redirect(request, 'lmn/users/user_profile.html')
+            return redirect('lmn:user_profile', user_pk=request.user.pk)
         else:
             messages.error(request, 'Please correct the error below.')
     else:
