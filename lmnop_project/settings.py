d9de605a712eaede140f085af5897cac6f5425ae
Dani <dani@dani.dani>
2019-11-18 18:03:30 -0600

    undid settings.py changes

--- lmnop_project/settings.py
+++ lmnop_project/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = '8c01$#j44g3znb)$q0()8)!%ts-jc)k12!a75-!63qb%bj=d4k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+if os.getenv('GAE_INSTANCE'):
+    ALLOWED_HOSTS = ['lmnop-259314.appspot.com']
+else:
+    ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -92,12 +94,12 @@ DATABASES = {
     
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
+        #'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jhfpkmbg',
-        'USER': 'jhfpkmbg',
-        'PASSWORD': os.environ['LMNOP_DB_PW'],
-        'HOST': 'salt.db.elephantsql.com',
-        'PORT': '5432',
+        #'USER': 'jhfpkmbg',
+        #'PASSWORD': os.environ['LMNOP_DB_PW'],
+        #'HOST': 'salt.db.elephantsql.com',
+        #'PORT': '5432',
     }
 }
 
@@ -140,9 +142,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Where to send user after successful login if no other page is provided.
 # Should provide the user object.
